3589060fb258990b4fb875ff4375b3926ef12943
Park Eun Bum <mpark6233@spc.co.kr>
2019-06-24 15:01:18 +0900

    UPDATE: SASS restructured

--- header.php
+++ header.php
@@ -16,7 +16,6 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
 	<script src="https://kit.fontawesome.com/ec23c08cf8.js"></script>
 	
@@ -107,7 +106,7 @@
 							wp_nav_menu( array(
 								'menu'           	=> '',
 								'theme_location'    => 'primary',
-								'depth'             => 3,
+								'depth'             => 2,
 								'container'         => '',
 								'container_class'   => 'collapse navbar-collapse',
 								'container_id'      => 'navbar-collapse-1',
